fdbff7b532d66cda9a66124bf1072464f152e8c9
Daniel Borges <danprog91@gmail.com>
2023-09-23 21:59:58 -0300

    Criação de um usuário

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,33 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.post('/users', async (request, replay) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
 
-prisma.user.create({
-  data: {
-    name: 'Daniel Borges',
-    email: 'danielborgesbr@gmail.com',
-  },
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return replay.status(201).send()
+})
+
+app.get('/users', async (request, replay) => {
+  const users = await prisma.user.findMany()
+
+  // console.log(users)
+
+  return replay.send({ users })
 })
