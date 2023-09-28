8b9ee5a930506d7af60c45970bef9ef4394a0865
Daniel Borges <danprog91@gmail.com>
2023-09-27 22:13:43 -0300

    Primeiro teste unitário

--- src/http/use-cases/register.spec.ts
+++ src/http/use-cases/register.spec.ts
@@ -1,5 +1,39 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+// import { PrismaUserRepository } from '@/repositories/prisma/prisma-users-repository'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(4)
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    // const prismaUserRepository = new PrismaUserRepository()
+    // const registerUseCase = new RegisterUseCase(prismaUserRepository)
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe_test@example.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHasshed = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHasshed).toBe(true)
+  })
 })
