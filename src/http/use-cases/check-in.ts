0cb13589aaf4f341819e52b88de74d12f570a302
Daniel Borges <danprog91@gmail.com>
2023-10-05 23:35:34 -0300

    Caso de uso de criação de academia

--- src/http/use-cases/check-in.ts
+++ src/http/use-cases/check-in.ts
@@ -3,6 +3,8 @@ import { GymsRepository } from '@/repositories/gyms-repository'
 import { CheckIn } from '@prisma/client'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
 import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
+import { MaxDistanceError } from './errors/max-distance-error'
+import { MaxNumberOfCheckInsError } from './errors/max-number-of-check-ins-error'
 
 interface CheckInUseCaseRequest {
   userId: string
@@ -33,7 +35,6 @@ export class CheckInUseCase {
       throw new ResourceNotFoundError()
     }
 
-    // TODO: calculate distance between user and gym
     const distance = getDistanceBetweenCoordinates(
       {
         latitude: userLatitude,
@@ -48,7 +49,7 @@ export class CheckInUseCase {
     const MAX_DISTANCE_IN_KILOMETERS = 0.1
 
     if (distance > MAX_DISTANCE_IN_KILOMETERS) {
-      throw new Error()
+      throw new MaxDistanceError()
     }
 
     const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
@@ -57,7 +58,7 @@ export class CheckInUseCase {
     )
 
     if (checkInOnSameDay) {
-      throw new Error()
+      throw new MaxNumberOfCheckInsError()
     }
 
     const checkIn = await this.checkInsRepository.create({
