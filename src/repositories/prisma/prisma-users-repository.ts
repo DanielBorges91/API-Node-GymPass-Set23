6eed6a9417f8d0577e071fade8ea9a62be1368b3
Daniel Borges <danprog91@gmail.com>
2023-10-06 23:59:18 -0300

    Repositórios do Prisma

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/lib/prisma'
-import { Prisma, User } from '@prisma/client'
+import { Prisma } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUserRepository implements UsersRepository {
@@ -11,7 +11,7 @@ export class PrismaUserRepository implements UsersRepository {
     return user
   }
 
-  async findByEmail(email: string): Promise<User | null> {
+  async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
         email,
@@ -21,7 +21,12 @@ export class PrismaUserRepository implements UsersRepository {
     return user
   }
 
-  findById(id: string): Promise<User | null> {
-    throw new Error('Method not implemented.')
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+    return user
   }
 }
