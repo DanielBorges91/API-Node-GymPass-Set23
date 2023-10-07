05b3f53811b5d7a8bac19b64f6f0292a62c7278d
Daniel Borges <danprog91@gmail.com>
2023-10-07 04:22:54 -0300

    Factories dos casos de uso

---
+++ src/http/use-cases/factories/make-fetch-nearby-gyms-use-case.ts
@@ -0,0 +1,9 @@
+import { FetchNearByUseCase } from '../fetch-nearby-gyms'
+import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
+
+export function makeFetchNearbyGymsUseCase() {
+  const gymsRepository = new PrismaGymsRepository()
+  const useCase = new FetchNearByUseCase(gymsRepository)
+
+  return useCase
+}
